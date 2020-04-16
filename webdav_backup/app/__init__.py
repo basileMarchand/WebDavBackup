64de8569c50c0bab1e78f6a3aec97d2e91fe71de
Basile MARCHAND <basile.marchand@gmail.com>
2020-04-16 08:55:15 +0200

    Add a main method for cli

--- webdav_backup/app/__init__.py
+++ webdav_backup/app/__init__.py
@@ -1,3 +1,21 @@
 
 from .backend import Backend
 from .cli import WDBCli
+
+from webdav_backup.settings import Credential, Config
+from webdav_backup.client  import Client
+
+
+def main():
+    config = Config()
+    credential = Credential()
+    client = Client()
+    client.connect(credential)
+    client.setConfig(config)
+
+    backend = Backend()
+    backend.client = client
+    backend.config = config
+    backend.credential = credential
+
+    WDBCli( backend )
